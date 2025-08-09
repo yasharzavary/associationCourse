748a08eab57bcda93ce2f35428cf4baa5f0a9aa8
yashar <115088770+yasharzavary@users.noreply.github.com>
2025-08-09 18:49:57 +0330

    feat: index app completely coded now first page backend code is ready

--- associationCoursePage/apps/index/views.py
+++ associationCoursePage/apps/index/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
-
-
+from django.conf import settings
+import json
 
 def index(request):
-    pass
+    # read course details from course.json file.
+    with open(f'{settings.BASE_DIR}{settings.FILES_URL}/information/courses.json', 'r') as f:
+        courses = json.load(f)
+    return render(request, 'index.html', {'courses': courses, 'files':settings.FILES_URL})
+
